3ae2d18f9a69f92557c26438d2cc3f65fc475e4f
Alex Cattell <abcattell91@gmail.com>
2022-10-23 14:42:14 +0100

    added mobile device scaling

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -29,26 +29,30 @@ const Portfolio = () => {
 
   const renderPortfolio = (portfolio) => {
     return (
-      <div className="images-container">
-        {
-          portfolio.map((port, idx) => {
-            return (
-              <div key={idx} className="image-box">
-                <img src={port.image} alt="portfolio cover"
-                  className="portfolio-image"
-                />
-                <div className="content">
-                  <p className="title">{port.name}</p>
-                  <h4 className="description">{port.description}</h4>
-                  <button className="button" onClick={() => {window.open(port.url)}}>View</button>
-                </div>
-              </div>
-            )
-          })
-        }
-      </div>
-    )
-  }
+        <div className="images-container">
+            {
+                portfolio.map((port, idx) => {
+                    return (
+                        <div className="image-box" key={idx}>
+                            <img
+                            src={port.image}
+                            className="portfolio-image"
+                            alt="portfolio" />
+                            <div className="content">
+                                <p className="title">{port.name}</p>
+                                <h4 className="description">{port.description}</h4>
+                                <button
+                                    className="btn"
+                                    onClick={() => window.open(port.url)}
+                                >View</button>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
+}
 
   return (
     <>
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,35 +1,41 @@
 import { Link, NavLink } from 'react-router-dom';
+import { useState } from 'react';
 import './index.scss'; // index from inside sidebar folder
 import lettera from '../../assets/images/letter-a.png';
 import logoSub from '../../assets/images/logoSub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faHome, faUser, faEnvelope, faSuitcase, faGear } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUser, faEnvelope, faSuitcase, faGear, faBars, faClose } from '@fortawesome/free-solid-svg-icons'
 
-const Sidebar = () => (
-  <div className='nav-bar'>
-    <Link className='logo' to='/'>
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  return (
+    <div className="nav-bar">
+      <Link className='logo' to='/'>
       <img src={lettera} alt="logo" />
       <img className='sublogo' src={logoSub} alt="logoSubtitle" />
     </Link>
-    <nav>
-      <NavLink exact="true" activeclassname='active' to="/">
+      <nav className={showNav ? 'mobile-show' : ''}>
+      <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname='active' to="/">
         <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
       </NavLink>
 
-      <NavLink exact="true" activeclassname='active' className='about-link' to="/about">
+      <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname='active' className='about-link' to="/about">
         <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
       </NavLink>
 
-      <NavLink exact="true" activeclassname='active' className='contact-link' to="/contact">
+      <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname='active' className='contact-link' to="/contact">
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
 
-      <NavLink exact="true" activeclassname='active' className='portfolio-link' to="/portfolio">
+      <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname='active' className='portfolio-link' to="/portfolio">
         <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
       </NavLink>
-    </nav>
-    <ul>
+
+      <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faClose} color="#4d4d4e" size="3x" className="close-icon" />
+      </nav>
+      <ul>
       <li>
         <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/alexandercattell/">
           <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" className="anchor-icon" />
@@ -45,8 +51,15 @@ const Sidebar = () => (
           <FontAwesomeIcon icon={faGear} color="#4d4d4e" className="anchor-icon" />
         </NavLink>
       </li>
-    </ul>
-  </div>
-)
+      </ul>
+      <FontAwesomeIcon
+          onClick={() => setShowNav(true)}
+          icon={faBars}
+          color="#ffd700"
+          size="3x"
+          className='hamburger-icon' />
+    </div>
+  )
+}
 
-export default Sidebar;
+export default Sidebar
