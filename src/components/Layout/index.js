63a1da0e50f371bdea5b7de98bd9709eb6fb08bf
Alex Cattell <abcattell91@gmail.com>
2022-11-01 17:07:00 +0000

    updated variable themes

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,24 +2,12 @@ import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar';
 import './index.scss'; // index from inside layout folder
 
+
 const Layout = () => {
   return (
   <div className="App">
     <Sidebar />
-    <div className="page">
-      <span className="tags top-tags">
-        <span className="top-tag-html">&lt;html&gt;</span><br />
-        <span className="top-tag-body">&lt;body&gt;</span>
-      </span>
-
-      <Outlet />
-
-      <span className="tags bottom-tags">
-      &lt;body&gt;
-      <br />
-      <span className="bottom-tag-html">&lt;/html&gt;</span>
-      </span>
-    </div>
+    <Outlet />
   </div>
   )
 };
